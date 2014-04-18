49bf98eebd9f2c0d8cb3c5278f1d51429cb5466a
Jonathan Chang <jonoschang7@gmail.com>
2014-04-18 17:44:57 -0400

    Work on 7 actions for restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,20 +1,35 @@
 class RestaurantsController < ApplicationController
 	def index
+		@restaurants = Restaurant.all
 	end
 
 	def show
+		@restaurant = Restaurant.find(params[:id])
 	end
 
 	def new
+		@restaurant = Restaurant.new
 	end
 
 	def create
+		@restaurant = Restaurant.new(restaurant_params)
+		if @restaurant.save
+			redirect_to restaurants_path
+		else
+			render :new
+		end
 	end
 
 	def edit
+		@restaurant = Restaurant.find(params[:id])
 	end
 
 	def update
+		@restaurant = Restaurant.find(params[:id])
+		if @restaurant.update_attributes(restaurant_params)
+			redirect_to restaurant_path(@restaurant)
+		else
+			redirect_to restaurant_url(@restaurant)
 	end
 
 	def destroy
@@ -22,5 +37,6 @@ class RestaurantsController < ApplicationController
 
 	private
 	def restaurant_params
+		params.require(:restaurant).permit(:name, :capacity)
 	end
 end
