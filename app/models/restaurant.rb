18db0e56d7ecd02b98455d2c5fc8f576b5998367
Jonathan Chang <jonoschang7@gmail.com>
2014-04-18 18:06:35 -0400

    Change association from many to many to has many belongs to

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,5 @@
 class Restaurant < ActiveRecord::Base
 	has_many :reservations
-	has_many :users, :through => :reservations
 
 	def availability(party_size, party_time)
 		reserve_size = reservations.where(party_time: party_time).sum(:party_size)
