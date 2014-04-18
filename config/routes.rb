b56f94eff3c893863b8a65dc28c05cc1974a8b34
Jonathan Chang <jonoschang7@gmail.com>
2014-04-18 19:27:49 -0400

    Create routes for reservations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Openseat2::Application.routes.draw do
-  resources :restaurants
+  resources :restaurants do
+    resources :reservations, :except => [:index]
+  end
   resources :users, :except => [:index, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
