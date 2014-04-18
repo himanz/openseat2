e8932f89a4de4cae81ad4443a9e3dcc2e6d9169e
Jonathan Chang <jonoschang7@gmail.com>
2014-04-18 12:38:14 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Openseat2::Application.config.secret_key_base = '244ee6e363f3644f7480c2640c0655da0f086a14372007bcc37b16c43f36e3aa62a1623738ca7334c17a16d03f35cc83312bd1648d0e22f4ab541b1f2d451110'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Openseat2::Application.config.session_store :cookie_store, key: '_openseat2_session'
