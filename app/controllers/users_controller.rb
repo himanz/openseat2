1cc4ec9c9a66938b797537b8d2ab5624f660a120
Jonathan Chang <jonoschang7@gmail.com>
2014-04-19 00:09:02 -0400

    Edit and create action for user with edit view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,9 +17,16 @@ class UsersController < ApplicationController
 	end
 
 	def edit
+		@user = User.find(params[:id])
 	end
 
 	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(user_params)
+			redirect_to user_path(@user)
+		else
+			render :edit
+		end
 	end
 
 	def user_params
