4976237fb26da84fdcf3adebe7c65448b40c0b6e
Jonathan Chang <jonoschang7@gmail.com>
2014-04-19 15:52:33 -0400

    Add check box to new user form to see if user is owner, show new restaurant in index only to owners

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,6 @@ class UsersController < ApplicationController
 	end
 
 	def user_params
-		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+		params.require(:user).permit(:name, :email, :password, :password_confirmation, :owner)
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140419173525) do
+ActiveRecord::Schema.define(version: 20140419193332) do
 
   create_table "reservations", force: true do |t|
     t.integer  "restaurant_id"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20140419173525) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "owner"
   end
 
 end
