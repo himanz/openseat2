7424aef2d5327537705576b8f5d6a94e0377df12
Jonathan Chang <jonoschang7@gmail.com>
2014-04-19 22:19:52 -0400

    Change validation for user model party size greater than 0

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,7 +4,7 @@ class Reservation < ActiveRecord::Base
 
   validates :restaurant, presence: true
 	validate :availability
-	validates :party_size, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+	validates :party_size, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
 
 	private
 	def availability
