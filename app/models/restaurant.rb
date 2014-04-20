5695721edcc7fdf6c3198633d7a916d9ad5e924e
Jonathan Chang <jonoschang7@gmail.com>
2014-04-19 23:28:42 -0400

    Add validation presence to address

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,7 @@
 class Restaurant < ActiveRecord::Base
 	has_many :reservations
 
-	validates_presence_of :name
+	validates_presence_of :name, :address
 	validates :capacity, numericality: { only_integer: true } 
 
 	def availability(party_size, party_time)
