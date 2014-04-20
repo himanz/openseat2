bd071bbb2679bbdeb7d6280ea3f6ac944d21da01
Jonathan Chang <jonoschang7@gmail.com>
2014-04-19 23:32:08 -0400

    Password validation for user at least 6 char

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
 
   validates :name, :email, :presence => true
   validates :email, uniqueness: true
+  validates :password, length: { minimum: 6 }
 	has_secure_password
 end
